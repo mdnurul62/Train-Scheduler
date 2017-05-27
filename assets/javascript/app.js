0b28b54b595e81a7d8bd987e3bee651c1ac48c93
mdnurul62 <nurul.alam1@hotmail.com>
2017-05-26 20:44:54 -0400

    Final version of train scheduler

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,11 @@
 //alert("Hello");
+//Sets clock on the page
+setInterval(function() {
+	var presentTime = moment().format("HH:mm:ss");
+	$("#current-time").html(presentTime);
+}, 1000);
+	
+
 
 // Initialize Firebase
   var config = {
@@ -17,10 +24,10 @@
 
 
 	//Initial variables to contain data in form
-  	var trainName 	= "";
-  	var destination = "";
-  	var firstTime 	= "";
-  	var frequency 	= "";
+  	var trainName 	= "Acela Express";
+  	var destination = "Washington D.C";
+  	var firstTime 	= "00:30";
+  	var frequency 	= "15";
 
 
 	//jQuery on click statement to capture input data by clicking a button with ID "add-train-name" to store data in variables
@@ -32,28 +39,20 @@
   	var destination = $("#destination-input").val().trim();
   	var firstTime 	= $("#time-input").val().trim();
   	var frequency 	= $("#frequency-input").val().trim();
-	// Create a new variable as object containing sets of property and value
-  	var train = {
+	
+  	
+	//Uploading train data to the database by using push method to store data everytime in the database
+  	database.ref().push({
   		name: trainName,
   		destination: destination,
   		time: firstTime,
   		frequency: frequency,
-  		dateAdded: firebase.database.ServerValue.TIMESTAMP
-  	};
-
-  	console.log(trainName);
-  	console.log(destination);
-  	console.log(firstTime);
-  	console.log(frequency);
-  	//return false;
-  	
-	//Uploading train data to the database by using push method to store data everytime in the database
-  	database.ref().push(train);
-  		console.log(train.name);
-  		console.log(train.destination);
-  		console.log(train.time);
-  		console.log(train.frequency);
-  		//Alert
+  		//dateAdded: firebase.database.ServerValue.TIMESTAMP
+  	});
+  		console.log(name);
+  		console.log(destination);
+  		console.log(time);
+  		console.log(frequency);
   		//alert("Train added!");
   		//return false;
   	
@@ -63,40 +62,60 @@
   	$("#time-input").val("");
   	$("#frequency-input").val("");
 
-  	//return false;
-
-
   });
 
   // Firebase watcher + initial loader which is to grab the data and post into user area by using firebase listerner
   database.ref().on("child_added", function(snapshot) {
   		// Log everything that's coming out of snapshot
   		console.log(snapshot.val());
-  		var newTrain = snapshot.val();
-  		console.log(newTrain);
-  		var tFrequency = newTrain.frequency;
+  		var newTrainInfo = snapshot.val();
+  		console.log(newTrainInfo);
+  		var tFrequency = newTrainInfo.frequency;
   		console.log(tFrequency);
 
-        var firstTime = newTrain.time;
-        console.log(firstTime);
-        // First Time (pushed back 1 year to make sure it comes before current time)
+      var firstTime = newTrainInfo.time;
+      console.log(firstTime);
+      // First Time (pushed back 1 year to make sure it comes before current time)
 
-        var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
+      var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
 
-        // Current Time
-        var currentTime = moment();
+      // Current Time
+      var currentTime = moment();
 
-        // Difference between the times
-        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+      // Difference between the times
+      var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+      console.log(diffTime);
 
-        // Time apart (remainder)
-        var tRemainder = diffTime % tFrequency;
+      // Time apart (remainder)
+      var tRemainder = diffTime % tFrequency;
 
-        // Minute Until Train
-        var tMinutesTillTrain = tFrequency - tRemainder;
+      // Minute Until Train
+      var tMinutesTillTrain = tFrequency - tRemainder;
 
-        // Next Train
-        var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-	//All user input will be posted in the user area
-  	$("tbody").append("<tr><td>" + newTrain.name + "</td><td>" + newTrain.destination + "</td><td>" + newTrain.frequency + "</td><td>" + moment(nextTrain).format("hh:mm") + "</td><td>" + tMinutesTillTrain + "</td></tr>");
-    });
+      // Next Train
+      var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+
+
+      //creates a data object to run through the loop
+  	  var trainSchedule = moment(firstTimeConverted).add(frequency, "minutes");
+  	  var minutesAway = moment(trainSchedule).add(currentTime, 'minutes');
+
+      //Adds frequency interval at every iteration until  the next train time is greater than the current time
+ 	    if (parseInt(trainSchedule) < parseInt(currentTime)){
+  		  //updates the loop train object
+  		  trainSchedule = moment(trainSchedule).add(frequency, 'minutes');
+  		  //updates the next train string 
+  		  nextTrain = moment(trainSchedule).format('HH:mm');
+  	  } 
+
+ 	    var minutesAway = moment(nextTrain).diff(currentTime, 'minutes');
+
+ 	    //displays the next arrival in military format
+ 	    var displayNextTrain = moment(trainSchedule).format('HH:mm');
+ 
+       
+	   //All user input will be posted in the user area
+  	 $("tbody").append("<tr><td>" + newTrainInfo.name + "</td><td>" + newTrainInfo.destination + "</td><td>" + newTrainInfo.frequency + "</td><td>" + displayNextTrain + "</td><td>" + minutesAway + "</td></tr>");
+  });
+
+  
